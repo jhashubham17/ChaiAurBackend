691942050f075bbe6c0fa3c66fa3bb07738407cd
Shubham jha <sjha870054@gmail.com>
2025-07-13 21:58:41 +0530

    ExpressConnected

--- VideoTube/src/db/index.js
+++ VideoTube/src/db/index.js
@@ -1,20 +1,29 @@
-// Import mongoose
+// Import mongoose – a popular library to work with MongoDB in Node.js
 import mongoose from "mongoose";
-// Import DB_NAME from constants
+
+// Import the DB_NAME constant – it contains the name of the database
 import { DB_NAME } from "../constants.js";
 
+// Define an async function to connect to the MongoDB database
 const connectDB = async () => {
   try {
+    // Try to connect to MongoDB using the connection string from environment variables
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
+
+    // If the connection is successful, print a success message with the database host
     console.log(
-      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+      `\n ✅ MongoDB connected! DB HOST: ${connectionInstance.connection.host}`
     );
   } catch (error) {
-    console.log("MONGODB CONNECTION ERROR", error);
+    // If there's an error while connecting, print an error message
+    console.log("❌ MongoDB connection error:", error);
+
+    // Exit the process with failure code (1) so that the app doesn't continue running
     process.exit(1);
   }
 };
 
+// Export the connectDB function so it can be used in other files
 export default connectDB;

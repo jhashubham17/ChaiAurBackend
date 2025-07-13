830ddaa44a3be710de71813b43872b1160aed73e
Shubham jha <sjha870054@gmail.com>
2025-07-14 01:25:02 +0530

    bcrypt&jsonwebtoken-Install

---
+++ VideoTube/src/models/video.model.js
@@ -0,0 +1,47 @@
+import mongooes, {Schema} from mongooes;
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const userSchema = new Schema({
+videoFile: {
+            type: String, //cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true
+        },
+        title: {
+            type: String, 
+            required: true
+        },
+        description: {
+            type: String, 
+            required: true
+        },
+        duration: {
+            type: Number, 
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+},
+{
+    timestamps: true
+}
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongooes.model("video, videoSchema")
